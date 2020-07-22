adf8c0725df08f18fafb08aac0b2455ee49581f4
saicharan67 <mahankalisaicharan@gmail.com>
2020-07-22 18:31:59 +0530

    4 commit

--- contact.php
+++ contact.php
@@ -1,25 +1,26 @@
 <?php
   if(isset($_POST['submit'])) {
-	$Name = $_POST['name'];
+	$Name = $_REQUEST['name'];
 	
 	
-	$email = $_POST['email'];
+	$email = $_REQUEST['email'];
 	
-	$subject = $_POST['subject'];
-	$message = $_POST["message"];
+	$subject = $_REQUEST['subject'];
+	$message = $_REQUEST["message"];
 
-	if (empty($Name)) || (empty($email)) || (empty($subject)) || (empty($message)){
+	if (empty($Name) || empty($email) || empty($subject) || empty($message)){
 
 
 		echo "Fill All";
 	}
 	else{
 
-		mail("mahankalisaicharan@gmail.com" , $subject , $message "From: $name <$email> ");
+		mail("mahankalisaicharan@gmail.com" , $subject , $message , "From: $name <$email> ");
 		echo "<script type='text/javascript' charset='utf-8' >
 		alert('message sent');
 		window.history.log(-1);
 		</script>";
+	
+      }
 	}
-}
 ?>
